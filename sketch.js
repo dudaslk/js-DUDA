f662806e3093536a3ddd346701b9d2882b4ae537
Eduarda Ferreira <ferreira.eduarda23@escola.pr.gov.br>
2024-09-25 20:24:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,36 @@
-r;
+let cor;
 let posicaohorizontal;//x
 let posicaovertical;//y
 function setup() {
-  createCanvas(400, 400);
-  background(color(100,0,0));
-  cor =color(random(0,255),random(0,255),random(0,255));
-  posicaohorizontal = 200
-  posicaovertical = 200
+  createCanvas(1000, 800);
+background(color(100,0,100));
+    cor= color(random(0,255),random(0,255),random(0,255));
+  posicaohorizontal=200
+  posicaovertical=200
 }
-
 
 function draw() {
   fill(cor);
-  circle(posicaohorizontal, posicaovertical, 50);
+  circle(posicaohorizontal,posicaovertical,50);
   if(mouseX<posicaohorizontal){
-    posicaohorizontal = posicaohorizontal -1;
+  posicaohorizontal=posicaohorizontal-1;
     
-  }
-    if(mouseX>posicaohorizontal) {
-     posicaohorizontal = posicaohorizontal +1; 
-      
-    }
-    if(mouseY<posicaovertical) {
-      posicaovertical--;
-    }
-    if(mouseY>posicaovertical) {
-      posicaovertical++;
-    }
+ }
+ if(mouseY>posicaohorizontal){
+ posicaohorizontal=posicaohorizontal+1;
   
-  if(mouseIsPressed){
-  cor = color(random(0,255),random(0,255),random(0,255),random(0,100));
+ }
+ if(mouseY<posicaovertical){
+ posicaovertical--;
+  
+ }
+ if(mouseY>posicaovertical){
+ posicaovertical++;
+ }
+   cor= color(random(0,255),random(0,255),random(0,255),random(0,100));
+              
   
 }
-
 
   
   
